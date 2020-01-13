659b00a0f57f43453f7072abe1485483687491b2
ai73aaa <1qazxcvBG>
2020-01-13 18:59:32 +0300

    project assembly

---
+++ src/neko-2-features/features-4-social/social-1-neko/neko-1-ui/Neko.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {FlexColumnCenterCenter} from "../../../../neko-3-styles/flex-containers";
+
+interface INekoProps {
+    loading: boolean;
+    error: string;
+
+    name: string;
+
+    logoutCallback: () => void;
+}
+
+const Neko: React.FC<INekoProps> = (
+    {
+        loading, error,
+
+        name,
+
+        logoutCallback,
+    }
+) => {
+
+    console.log('render Neko');
+    return (
+        <div
+            style={{
+                ...FlexColumnCenterCenter,
+                height: '80vh',
+            }}
+        >
+            neko
+
+            {loading
+                ? <div style={{color: 'orange'}}>loading...</div>
+                : error
+                    ? <div style={{color: 'red'}}>{error}</div>
+                    : name
+                        ? <div style={{color: 'lime'}}>{name}</div>
+                        : <div><br/></div>
+            }
+
+            <button onClick={logoutCallback} disabled={loading}>logout</button>
+        </div>
+    );
+};
+
+export default Neko;
