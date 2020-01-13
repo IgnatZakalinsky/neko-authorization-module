5b6dcfe518cbfad01f2a4d0ff41a90111f375f68
ai73aaa <1qazxcvBG>
2020-01-13 20:07:53 +0300

    Updates

---
+++ precache-manifest.6631e4d9e80ca685560834266b75619f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d545e654445e51298dfea9bef67042f",
+    "url": "/neko-authorization-module/index.html"
+  },
+  {
+    "revision": "9202ec9d3d399684a47f",
+    "url": "/neko-authorization-module/static/css/main.8a48f644.chunk.css"
+  },
+  {
+    "revision": "c32af963acc9f375571c",
+    "url": "/neko-authorization-module/static/js/2.d0128fd4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/neko-authorization-module/static/js/2.d0128fd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9202ec9d3d399684a47f",
+    "url": "/neko-authorization-module/static/js/main.4267bae6.chunk.js"
+  },
+  {
+    "revision": "eae7df98d94175495c92",
+    "url": "/neko-authorization-module/static/js/runtime-main.6d94ecc1.js"
+  }
+]);
