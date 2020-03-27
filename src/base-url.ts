3ce6ce5fdbb6b93c8ff0de60c5c0cd5509544592
ai73aaa <1qazxcvBG>
2020-03-27 18:41:31 +0300

    add new back

--- src/base-url.ts
+++ src/base-url.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = 'https://dry-forest-56016.herokuapp.com/';
+export const baseURL = 'https://neko-cafe-back.herokuapp.com/';
 
 export const instance = axios.create({
     baseURL
